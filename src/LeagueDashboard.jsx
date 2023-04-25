c1305fda81b486e3172050c179d2a13291a0807f
“Lissa <lwarshaw674@gmail.com>
2023-04-25 11:21:01 -0400

    update to add team

--- src/LeagueDashboard.jsx
+++ src/LeagueDashboard.jsx
@@ -1,14 +1,17 @@
 import React, { useState, useEffect } from "react";
-import Select from "react-select";
 import "./LeagueDashboard.css";
 import AddTeamForm from "./AddTeam.jsx";
+import Select from "react-select";
 
-const LeagueCard = ({ league, onDelete }) => {
+const LeagueCard = ({ league, onDelete, onDeleteTeam, onAdd }) => {
   const handleDelete = async () => {
     try {
-      const response = await fetch(`https://ancient-coast-33215.herokuapp.com/football/${league._id}`, {
-        method: "DELETE",
-      });
+      const response = await fetch(
+        `https://ancient-coast-33215.herokuapp.com/football/${league._id}`,
+        {
+          method: "DELETE",
+        }
+      );
       if (response.ok) {
         onDelete(league._id);
       }
@@ -17,9 +20,48 @@ const LeagueCard = ({ league, onDelete }) => {
     }
   };
 
+  const handleDeleteTeam = async (teamId) => {
+    try {
+      const response = await fetch(
+        `https://ancient-coast-33215.herokuapp.com/football/teams/${teamId}`,
+        {
+          method: "DELETE",
+        }
+      );
+      if (response.ok) {
+        onDeleteTeam(teamId);
+      }
+    } catch (error) {
+      console.error("Error deleting team:", error);
+    }
+  };
+
+  const handleAddTeam = async (newTeam) => {
+    try {
+      const response = await fetch(
+        `https://ancient-coast-33215.herokuapp.com/football/teams`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newTeam),
+        }
+      );
+      if (response.ok) {
+        const data = await response.json();
+        onAdd(data);
+      }
+    } catch (error) {
+      console.error("Error adding team:", error);
+    }
+  };
+
   return (
     <div className="league-card">
-      <button className="delete-btn" onClick={handleDelete}>X</button>
+      <button className="delete-btn" onClick={handleDelete}>
+        X
+      </button>
       {league.area?.flag && (
         <img src={league.area.flag} alt={`${league.area?.name} flag`} />
       )}
@@ -36,6 +78,21 @@ const LeagueCard = ({ league, onDelete }) => {
       <p>
         <strong>End Date:</strong> {league.currentSeason?.endDate}
       </p>
+      <AddTeamForm leagueId={league._id} onAdd={handleAddTeam} />
+      <div className="team-list">
+        {league.teams?.map((team) => (
+          <div key={team._id} className="team">
+            <button
+              className="delete-btn"
+              onClick={() => handleDeleteTeam(team._id)}
+            >
+              X
+            </button>
+            <p>{team.name}</p>
+            <p>{team.venue}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
@@ -76,19 +133,38 @@ const LeagueDashboard = () => {
   }, []);
 
   const handleDeleteLeague = (id) => {
-    setLeagues((prevLeagues) => prevLeagues.filter((league) => league._id !== id));
+    setLeagues((prevLeagues) =>
+      prevLeagues.filter((league) => league._id !== id)
+    );
   };
 
-  const handleAddTeam = (newTeam) => {
-    setLeagues((prevLeagues) =>
-      prevLeagues.map((league) => {
-        if (league._id === newTeam.leagueId) {
-          return { ...league, teams: [...league.teams, newTeam] };
-        } else {
-          return league;
+  const handleAddTeam = async (newTeam) => {
+    try {
+      const response = await fetch(
+        "https://ancient-coast-33215.herokuapp.com/football/teams",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(newTeam),
         }
-      })
-    );
+      );
+      if (response.ok) {
+        const data = await response.json();
+        setLeagues((prevLeagues) =>
+          prevLeagues.map((league) => {
+            if (league._id === newTeam.leagueId) {
+              return { ...league, teams: [...league.teams, data] };
+            } else {
+              return league;
+            }
+          })
+        );
+      }
+    } catch (error) {
+      console.error("Error adding team:", error);
+    }
   };
 
   const filteredLeagues = leagues.filter((league) => {
@@ -98,17 +174,20 @@ const LeagueDashboard = () => {
 
     switch (selectedOption.value) {
       case "name":
-        return league.name
-          ?.toLowerCase()
-          .includes(searchTerm.toLowerCase());
+        return (
+          league.name &&
+          league.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
       case "code":
-        return league.code
-          ?.toLowerCase()
-          .includes(searchTerm.toLowerCase());
+        return (
+          league.code &&
+          league.code.toLowerCase().includes(searchTerm.toLowerCase())
+        );
       case "area":
-        return league.area?.name
-          ?.toLowerCase()
-          .includes(searchTerm.toLowerCase());
+        return (
+          league.area &&
+          league.area.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
       default:
         return false;
     }
@@ -127,8 +206,13 @@ const LeagueDashboard = () => {
       </div>
       <div className="league-cards-container">
         {filteredLeagues.map((league) => (
-          <LeagueCard key={league._id} league={league} onDelete={handleDeleteLeague} />
-          ))}
+          <LeagueCard
+            key={league._id}
+            league={league}
+            onDelete={handleDeleteLeague}
+            onAdd={handleAddTeam}
+          />
+        ))}
       </div>
     </div>
   );
