99e35de081c00bcb24bcc5d166c33a628c99810b
“Lissa <lwarshaw674@gmail.com>
2023-04-25 10:35:11 -0400

    css and more functionality

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ const App = () => {
 
   return (
     <div className="app">
-      <h1>Current Season Details</h1>
+      <h1 style={{ color: 'black', textAlign: "center", marginBottom: "50px" }}>Current Season Details</h1>
       <LeagueDashboard leagues={leagues} />
     </div>
   );
--- src/LeagueDashboard.jsx
+++ src/LeagueDashboard.jsx
@@ -1,13 +1,28 @@
-import React from "react";
-import './LeagueDashboard.css';
+import React, { useState, useEffect } from "react";
+import Select from "react-select";
+import "./LeagueDashboard.css";
 
 
-//The dashboard consists of a collection of LeagueCard components, each displaying information for a single league, such as the league's name, code, area, start date, and end date. The LeagueDashboard component will be responsible for rendering these LeagueCard components, and possibly for filtering or sorting the data as well.
+const LeagueCard = ({ league, onDelete }) => {
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`https://ancient-coast-33215.herokuapp.com/football/${league._id}`, {
+        method: "DELETE",
+      });
+      if (response.ok) {
+        onDelete();
+      }
+    } catch (error) {
+      console.error("Error deleting league:", error);
+    }
+  };
 
-const LeagueCard = ({ league }) => {
   return (
     <div className="league-card">
-      <img src={league.area?.flag} alt={`${league.area?.name} flag`} />
+      <button className="delete-btn" onClick={handleDelete}>X</button>
+      {league.area?.flag && (
+        <img src={league.area.flag} alt={`${league.area?.name} flag`} />
+      )}
       <h2>{league.name}</h2>
       <p>
         <strong>Code:</strong> {league.code}
@@ -25,14 +40,80 @@ const LeagueCard = ({ league }) => {
   );
 };
 
+const LeagueDashboard = () => {
+  const [leagues, setLeagues] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [selectedOption, setSelectedOption] = useState(null);
 
+  const options = [
+    { value: "name", label: "Name" },
+    { value: "code", label: "Code" },
+    { value: "area", label: "Area" },
+  ];
 
-const LeagueDashboard = ({ leagues }) => {
+  const handleChange = (selectedOption) => {
+    setSelectedOption(selectedOption);
+  };
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  useEffect(() => {
+    const fetchLeagues = async () => {
+      try {
+        const response = await fetch(
+          "https://ancient-coast-33215.herokuapp.com/football"
+        );
+        const data = await response.json();
+        setLeagues(data);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchLeagues();
+  }, []);
+
+  const filteredLeagues = leagues.filter((league) => {
+    if (!selectedOption) {
+      return false;
+    }
+
+    switch (selectedOption.value) {
+      case "name":
+        return league.name
+          ?.toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      case "code":
+        return league.code
+          ?.toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      case "area":
+        return league.area?.name
+          ?.toLowerCase()
+          .includes(searchTerm.toLowerCase());
+      default:
+        return false;
+    }
+  });
+
   return (
     <div className="league-dashboard">
-      {leagues.map((league) => (
-        <LeagueCard key={league._id} league={league} />
-      ))}
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search for a league..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+        <Select options={options} onChange={handleChange} />
+      </div>
+      <div className="league-cards-container">
+        {filteredLeagues.map((league) => (
+          <LeagueCard key={league._id} league={league} />
+        ))}
+      </div>
     </div>
   );
 };
